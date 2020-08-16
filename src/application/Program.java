42de252036fc7cddc0d15e778b053a0f1e84457a
ec-ayrton <ec.ayrton@gmail.com>
2020-08-16 14:40:10 -0300

    primeira classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		position pos = new position(3,  5);
+		System.out.println(pos);
 	}
 
 }
