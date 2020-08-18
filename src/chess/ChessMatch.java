a82f4ee9d650dc6d46a2c752211dae9471b03be2
ec-ayrton <ec.ayrton@gmail.com>
2020-08-18 15:37:01 -0300

    implementando chessexception e chessposition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Roak;
 
@@ -24,12 +23,17 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece( char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).ToPosition() );
+		
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Roak(board, Color.WHITE), new Position(7, 0) );
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4) );
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4) );
+		placeNewPiece('a',8,new Roak(board, Color.WHITE));
+		placeNewPiece('e',1,new King(board, Color.WHITE));
+		placeNewPiece('e',8,new King(board, Color.BLACK));
 
-		 
+		
 	}
 	
 }
